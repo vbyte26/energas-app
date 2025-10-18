17a130e76177e882b42ea98c777fb06668ebf400
vbyte26 <jbmosta26@gmail.com>
2025-10-18 11:40:03 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loginBtn = document.getElementById("loginBtn");
-  if (loginBtn) {
-    loginBtn.addEventListener("click", () => {
-      // Redirigir a otra pantalla después del login
-      window.location.href = "consulta.html";
-    });
-  }
-});
